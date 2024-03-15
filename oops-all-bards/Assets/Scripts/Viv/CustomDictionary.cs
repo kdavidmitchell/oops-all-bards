7cb63700186fe0779c2e05f5661b16c27acbd75b
kdavidmitchell <kdavidmitchell@gmail.com>
2024-03-15 16:20:16 -0700

    adding viv core

--- oops-all-bards/Assets/Scripts/Viv/CustomDictionary.cs
+++ oops-all-bards/Assets/Scripts/Viv/CustomDictionary.cs
@@ -5,18 +5,34 @@ using Viv;
 
 public class CustomDictionary : MonoBehaviour
 {
-    [SerializeField] private List<SupertaskBinding> bindings = new List<SupertaskBinding>();
-    private Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+    [SerializeField] private List<SupertaskBindings> supertaskBindings = new List<SupertaskBindings>();
+    [SerializeField] private List<BehaviorBindings> behaviorBindings = new List<BehaviorBindings>();
+    private Dictionary<string, List<string>> supertaskDict = new Dictionary<string, List<string>>();
+    private Dictionary<string, List<string>> behaviorDict = new Dictionary<string, List<string>>();
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (var kvp in supertaskBindings) 
+        {
+            supertaskDict[kvp.key] = kvp.val;
+        }
+
+        foreach (var kvp in behaviorBindings)
+        {
+            behaviorDict[kvp.key] = kvp.val;
+        }
+
+        Debug.Log(supertaskDict["SabotagePlayer"]);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Dictionary<string, List<string>> SupertaskDict
     {
-        
+        get { return supertaskDict; }
+    }
+
+    public Dictionary<string, List<string>> BehaviorDict
+    {
+        get { return behaviorDict; }
     }
 }
